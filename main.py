813a7bed83b10f0a2a3ee801ed1d5074c1da0997
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:26 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# currency-converter
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Currency Converter using API in Python")
+    print("Project: currency-converter")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
